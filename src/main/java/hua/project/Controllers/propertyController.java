f9e57e31b293fc5ee56c515dafd46d90801b7240
Giorgos <giorgos2532018@yahoo.com>
2024-12-05 16:37:58 +0200

    i added property service&repo

---
+++ src/main/java/hua/project/Controllers/propertyController.java
@@ -0,0 +1,11 @@
+package hua.project.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("property")
+public class propertyController {
+
+
+}
---
+++ src/main/java/hua/project/Service/PropertyService.java
@@ -0,0 +1,35 @@
+package hua.project.Service;
+
+import hua.project.Entities.Property;
+import hua.project.Repository.PropertyRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PropertyService {
+
+private PropertyRepository propertyRepository;
+
+public PropertyService(PropertyRepository propertyRepository) {
+    this.propertyRepository = propertyRepository;
+}
+
+@Transactional
+public List<Property> getAllProperty() {
+    return propertyRepository.findAll();
+}
+
+@Transactional
+public void addProperty(Property property) {
+    propertyRepository.save(property);
+}
+
+@Transactional
+public Property getPropertyById(Integer id) {
+    return propertyRepository.findById(id).get();
+}
+
+
+}
